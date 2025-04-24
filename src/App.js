b111373ebfb808af7ea09a85721404172c304078
pla1n <danilkuisis@gmail.com>
2025-04-24 19:40:03 +0300

    f

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import { Component } from 'react';
 import { TaskList } from './components/TaskList/TaskList';
 import todos from './todos.json';
 import { Dropdown } from './components/Dropdown/Dropdown';
+import { Header } from './components/Header/Header';
+import { Modal } from './components/Modal/Modal';
+
 
 class App extends Component {
   state = {
-    todos: todos
+    todos: todos,
+    showModal: false,
+  }
+
+  openModal= () => {
+    this.setState({showModal: true})
+  }
+
+  closeModal= () => {
+    this.setState({showModal: false})
   }
 
   deleteToDo = (toDoId) => {
@@ -19,6 +31,11 @@ class App extends Component {
     const {todos} = this.state
     return (
       <>
+      <Header openModal={this.openModal}/>
+      {
+        this.state.showModal && <Modal closeModal={this.closeModal}>kajfkjwq kajsjh jbhfhxu</Modal>
+      }
+      
       <TaskList onDeleteToDo={this.deleteToDo} todos={todos}/>
       <Dropdown/>
       </>
